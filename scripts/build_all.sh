a315121b5e9743cee87532d7f2fbdcbe015543f2
Aniel Alexa <aniel.alexa@luxonis.com>
2024-02-09 18:36:36 +0200

    Undo colcon symlink

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -8,4 +8,4 @@ cd $ROOT/spectacularai_ros2
 # sudo rosdep init
 rosdep update
 rosdep install -i --from-path src --rosdistro humble -y
-colcon build --cmake-args "$@" --symlink-install
+colcon build --cmake-args "$@"
