639d3e9241d6c415e772364039ba75f4549a01e3
Jerry Ylilammi <jerry.ylilammi@gmail.com>
2024-03-07 17:00:20 +0200

    Additional cleanup stuff

--- scripts/download_and_build_static.sh
+++ scripts/download_and_build_static.sh
@@ -19,11 +19,15 @@ else
     exit 1
 fi
 
+WORKDIR=$(pwd)
 SDK=$(realpath sai_sdk)
 
 # Remove temp files on exit
 cleanup_sdk () {
     rm -rf $SDK
+    find $WORKDIR -name 'libspectacularAI.a' -delete
+    find $WORKDIR -name 'spectacular*.gz' -delete
+    find $WORKDIR -name 'spectacular*.zip' -delete
     echo "Cleanup done (removed $SDK)"
 }
 trap cleanup_sdk EXIT
@@ -34,7 +38,7 @@ mkdir -p $SDK
 cd $SDK
 curl -L -H "Accept: application/octet-stream" -H "Authorization: Bearer ${GITHUB_RAE_PAT_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" -o spectacularai_sdk.zip https://api.github.com/repos/spectacularAI/sdk-for-rae/releases/assets/${SPECTACULARAI_SDK_RELEASE_ID}
 unzip spectacularai_sdk.zip
-cd ..
+cd $WORKDIR
 
 # Build ROS node
 
